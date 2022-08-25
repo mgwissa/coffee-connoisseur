48ac4b8736fedfbce74a00b6464bb67455c1058c
Mike Wissa <wissam@detroittrading.com>
2022-08-25 16:52:39 -0400

    updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -26,6 +28,22 @@ export default function Home(props) {
     UseTrackLocation();
 
   console.log({ latLong, locationErrorMsg });
+
+  useEffect(() => {
+    async () => {
+      if (latLong) {
+        try {
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong);
+          console.log({ fetchedCoffeeStores });
+          //set coffee stores
+        } catch (error) {
+          //set error
+          console.log({ error });
+        }
+      }
+    };
+  }, [latLong]);
+
   const handleOnBannerBtnClick = () => {
     console.log("hi banner button");
     handleTrackLocation();
